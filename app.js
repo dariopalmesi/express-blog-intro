5cd311155dc0784bfcf1cc463ac4ee4ebb6aba61
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-30 15:41:25 +0100

    upgrade commit

--- app.js
+++ app.js
@@ -6,3 +6,17 @@
 // La rotta relativa ai post dovrà chiamare la funzione index() dal controller dedicato ( controllers/posts.js )
 // Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
 // Testare nel browser.
+
+
+const express = require('express')
+const app = express()
+const host = 'http://127.0.0.1'
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('<h1>Benvenuto nel mio blog!</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${host}:${port}`)
+})
